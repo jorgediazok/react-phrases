0cd7769378fead35ed133060914585fbd57dd737
Jorge <jorgediazok@gmail.com>
2020-12-15 16:35:58 -0300

    now showing phrases on display

--- src/App.js
+++ src/App.js
@@ -3,31 +3,36 @@ import Client from './api';
 import './App.css';
 
 function App() {
-  const [phrases, setPhrases] = useState([]);
+  const [phrases, setPhrases] = useState('');
+
+  const getData = async () => {
+    try {
+      let response = await Client.getEntries({
+        content_type: 'maradonapp',
+      });
+      const data = await response.items;
+      let randomNum = Math.floor(Math.random() * data.length);
+      let randomPhrase = data[randomNum].fields.phrases;
+      console.log(randomPhrase);
+      setPhrases(randomPhrase);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        let response = await Client.getEntries({
-          content_type: 'maradonapp',
-        });
-        const data = await response.items;
-        console.log(data);
-        setPhrases(data);
-      } catch (err) {
-        console.log(err);
-      }
-    };
     getData();
   }, []);
+
+  const handlePhrases = () => {
+    getData();
+  };
 
   return (
     <div className="app">
       <div className="card">
-        {phrases.map((phrase) => (
-          <h1 className="heading">{phrase.fields.phrases}</h1>
-        ))}
-        <button className="button" onClick={phrases}>
+        {phrases}
+        <button className="button" onClick={handlePhrases}>
           <span>Dame un consejo Diego</span>
         </button>
       </div>
