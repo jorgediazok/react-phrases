f473efe9224578687f9b9e1b8edfacdb0a6f68b9
Jorge <jorgediazok@gmail.com>
2020-12-15 01:10:37 -0300

    trying first with random phrases

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [phrases, setPhrases] = useState('');
+
+  const fetchPhrase = async () => {
+    let response = await axios.get('https://api.adviceslip.com/advice');
+    let phrases = response.data.slip.advice;
+    console.log(phrases);
+    setPhrases(phrases);
+  };
+
+  useEffect(() => {
+    fetchPhrase();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Maradonapp</h1>
+      <div className="card">
+        <h1>{phrases}</h1>
+      </div>
     </div>
   );
 }
